35e2d91bec3606c5d11a519ba17ce8c8e262af04
Ashwin Siripurapu <ashwins1@stanford.edu>
2012-04-06 02:48:54 -0700

    added bson -> json stuff. confused about some type stuff (autoboxing of numbers to Data.Attoparsec.Number?)

--- jsonbson/parse_coord.hs
+++ jsonbson/parse_coord.hs
@@ -1,10 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Aeson
-import qualified Data.ByteString.Lazy as B
+import qualified Data.Bson as B
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.CompactString.UTF8 as CS
 import Control.Applicative
 import Control.Monad
+import qualified Data.Text as T
 
+instance ToJSON B.Value where
+  toJSON Null = Null
+  toJSON (Bool b) = Bool b
+  toJSON (String str) = String (T.pack $ CS.unpack str)
+  toJSON (Float f) = Number f
+  toJSON (Array a) = Array (fromList $ map toJSON a)
+  toJSON (Fun (Function f)) = String (pack $ unpack f)
+  toJSON (Int32 i) = Number (toInteger i)
+  toJSON (Int64 i) = Number (toInteger i)
+  toJSON (UTC time) = String (BS.pack $ show time)
+  toJSON (Symbol sym) = String (T.pack $ CS.unpack str)
+  toJSON (MD5 hash) = String (T.pack $ BS.unpack hash)
+  
+
+{-
 data Coord = Coord { x :: Double, y :: Double } deriving Show
 
 instance FromJSON Coord where
@@ -20,3 +38,4 @@ main = do json <- B.readFile "coord.json"
           case c of
             Nothing -> putStrLn "oh no!"
             Just coord -> putStrLn (show coord)
+-}
